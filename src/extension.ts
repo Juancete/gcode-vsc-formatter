fe031a46c864298494f3b50b08e7762a59752bcd
Juancete <juan.contardo@gmail.com>
2019-09-28 19:23:51 -0300

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.languages.registerDocumentFormattingEditProvider('gcode', {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			var coleccion: vscode.TextEdit[] = formatLine(document.lineAt(0));
+			for (let i = 1; i < document.lineCount; i++) {
+				coleccion = coleccion.concat(formatLine(document.lineAt(i)));
+			}
+			return coleccion;
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function formatLine(line: vscode.TextLine) {
+	var newString = intercalate("G")(line.text.toUpperCase());
+	newString = intercalate("X")(newString);
+	newString = intercalate("Y")(newString);
+	newString = intercalate("Z")(newString);
+	newString = intercalate("T")(newString);
+	newString = intercalate("S")(newString);
+	newString = removeSemicolons(newString);
+	newString = splitBySemicolons(newString);
+
+	return [vscode.TextEdit.delete(line.range), vscode.TextEdit.insert(line.range.start, newString)];
+
+}
+type intercalate = (character: string) => (text: string) => string;
+
+let intercalate: intercalate = (character: string) => (text: string) => {
+	var position: number;
+	if (text.charAt(0) === character) {
+		{
+			position = text.slice(1).indexOf(character);
+			if (position !== -1) { position++; }
+		}
+	}
+	else { position = text.indexOf(character); }
+
+	if (position !== -1 && text.charAt(position - 1) !== " ") {
+
+		return [text.slice(0, position), " "].join('').concat(intercalate(character)(text.slice(position)));
+	}
+	else { return text; }
+};
+
+function removeSemicolons(text: string): string {
+	if (text.slice(text.length - 1) === ";") {
+		return text.slice(0, -1);
+	}
+	else { return text; }
+}
+function splitBySemicolons(text: string): string {
+	var position = text.indexOf(";");
+	if (text.indexOf(";") !== -1) {
+		return [text.slice(0, position), "\n", splitBySemicolons(text.slice(position + 1)).trim()].join('');
+	}
+	else { return text; }
+}
+// this method is called when your extension is deactivated
+export function deactivate() { }
+
+const pipe = <R>(...fns: Array<(a: R) => R>) =>
+  fns.reduce((prevFn, nextFn) => value => nextFn(prevFn(value)));
