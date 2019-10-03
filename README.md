f27eace514b68f3f46ad7a7a4052f17eba45ebd6
Juancete <juan.contardo@gmail.com>
2019-10-03 11:06:45 -0300

    Update test suite

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ This is a formatter for G-Code language.
 
 ## Features
 
-
-293/5000
 The G-code formatter separates the G, M, S, X, Y and Z commands, transforms the text to uppercase, separates the lines by semicolons, removes semicolons at the end of each line and finally (but not least) format each operation with at least two digits (For example G1 -> G01).
 
 ![feature G-Code](images/example.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -21,9 +21,9 @@ export function activate(context: vscode.ExtensionContext) {
 export function formatLine(line: vscode.TextLine) {
 	const lettersDigit = ["G","M","T"];
 	const letters = lettersDigit.concat(["X","Y","Z","I", "J","S", "R", "F", "C"]);
-
+	intercalate("")("");
 	const formatter = pipe(...letters.map(intercalate),
-		...lettersDigit.map(twoDigits),
+		...lettersDigit.map(commandsWithTwoDigits),
 		removeSemicolons,
 		splitBySemicolons
 	);
@@ -33,7 +33,7 @@ export function formatLine(line: vscode.TextLine) {
 }
 type stringTransformer = (character: string) => (text: string) => string;
 
-let intercalate: stringTransformer = (character: string) => (text: string) => {
+export let intercalate: stringTransformer = (character: string) => (text: string) => {
 	var position: number;
 	if (text.charAt(0) === character) {
 		{
@@ -50,26 +50,26 @@ let intercalate: stringTransformer = (character: string) => (text: string) => {
 	else { return text; }
 };
 
-let twoDigits: stringTransformer = (character: string) => (text: string) => {
+export let commandsWithTwoDigits: stringTransformer = (character: string) => (text: string) => {
 	var position = text.indexOf(character);
 	if(position !== -1){
 		const value1 = isNaN(+text.charAt(position + 2 ));
 		const value2 = text.charAt(position + 2);
 		if (isNaN(+text.charAt(position + 2 )) || text.charAt(position + 2) === " "){
-			return [text.slice(0, position+1), "0"].join('').concat(twoDigits(character)(text.slice(position+1)));
+			return [text.slice(0, position+1), "0"].join('').concat(commandsWithTwoDigits(character)(text.slice(position+1)));
 		}
-		else{return text.slice(0, position+1).concat(twoDigits(character)(text.slice(position+1)));}
+		else{return text.slice(0, position+1).concat(commandsWithTwoDigits(character)(text.slice(position+1)));}
 	}
 	return text;
 };
 
-function removeSemicolons(text: string): string {
+export function removeSemicolons(text: string): string {
 	if (text.slice(text.length - 1) === ";") {
-		return text.slice(0, -1);
+		return removeSemicolons(text.slice(0, -1));
 	}
 	else { return text; }
 }
-function splitBySemicolons(text: string): string {
+export function splitBySemicolons(text: string): string {
 	var position = text.indexOf(";");
 	if (text.indexOf(";") !== -1) {
 		return [text.slice(0, position), "\n", splitBySemicolons(text.slice(position + 1)).trim()].join('');
