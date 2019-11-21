773264ed94c1c48005b31dba536fcd666c3a19fa
Juancete <juan.contardo@gmail.com>
2019-11-21 11:16:20 -0300

    code refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,15 @@
 import * as vscode from 'vscode'
 
+type stringTransformer = (character: string) => (text: string) => string
+
+const charactersFromCodeWithDigits = ['G', 'M', 'T']
+const characters = charactersFromCodeWithDigits.concat(['X', 'Y', 'Z', 'I', 'J', 'S', 'R', 'F', 'C'])
+
 export function activate(context: vscode.ExtensionContext) {
 	const block = {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
 			var coleccion: vscode.TextEdit[] = formatLine(document.lineAt(0))
+
 			for (let i = 1; i < document.lineCount; i++) {
 				coleccion = coleccion.concat(formatLine(document.lineAt(i)))
 			}
@@ -11,27 +17,24 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}
 
-	let disposable = vscode.languages.registerDocumentFormattingEditProvider('gcode', block)
-	let otherDisposable = vscode.languages.registerDocumentFormattingEditProvider('centroid-gcode', block)
+	const disposable = vscode.languages.registerDocumentFormattingEditProvider('gcode', block)
+	const otherDisposable = vscode.languages.registerDocumentFormattingEditProvider('centroid-gcode', block)
 
 	context.subscriptions.push(disposable)
 	context.subscriptions.push(otherDisposable)
 }
 
 export function formatLine(line: vscode.TextLine) {
-	const lettersDigit = ["G", "M", "T"]
-	const letters = lettersDigit.concat(["X", "Y", "Z", "I", "J", "S", "R", "F", "C"])
-	const formatter = pipe(...letters.map(intercalate),
-		...lettersDigit.map(commandsWithTwoDigits),
+	const formatter = pipe(...characters.map(intercalate),
+		...charactersFromCodeWithDigits.map(commandsWithTwoDigits),
 		trimSpaces,
 		removeSemicolons,
 		splitBySemicolons
 	)
-	var newString = formatter(line.text.toUpperCase())
+	const newString = formatter(line.text.toUpperCase())
 	return [vscode.TextEdit.delete(line.range), vscode.TextEdit.insert(line.range.start, newString)]
 
 }
-type stringTransformer = (character: string) => (text: string) => string
 
 export let intercalate: stringTransformer = (character: string) => (text: string) => {
 	var position: number
@@ -43,53 +46,32 @@ export let intercalate: stringTransformer = (character: string) => (text: string
 	}
 	else { position = text.indexOf(character) }
 
-	if (position !== -1 && text.charAt(position - 1) !== " " && text.charAt(position - 1) !== ",") {
+	if (position !== -1 && text.charAt(position - 1) !== ' ' && text.charAt(position - 1) !== ',') {
+		return text.slice(0, position).concat(' ').concat(intercalate(character)(text.slice(position)))
+	} 
+	return text
+}
+const evaluateValidPosition = (position: number, aFunction: ()=>string , invalidString: string ) : string => (position !== -1) ? aFunction() : invalidString
 
-		return [text.slice(0, position), " "].join('').concat(intercalate(character)(text.slice(position)))
-	}
-	else { return text }
+export function splitBySemicolons(text: string): string {
+	const position = text.indexOf(';')
+	return evaluateValidPosition(position,() => [text.slice(0, position), '\n', splitBySemicolons(text.slice(position + 1)).trim()].join(''),text)
 }
 
 export function trimSpaces(text: string): string {
-	var position = text.indexOf(" ")
-	if (position !== -1) {
-		if ( text.charAt(position + 1) === " "){
-			const primerParte = text.slice(0, position)
-			const segundaParte =  text.slice(position+1)
-			return text.slice(0, position).concat(trimSpaces(text.slice(position+1)))
-		}
-		else {
-			const sigo = text.slice(position)
-			return text.slice(0, position+1).concat(trimSpaces(text.slice(position+1)))
-		}
-	}
-	return text
+	const position = text.indexOf(' ')
+	const evaluate = () => text.slice(0, (text.charAt(position + 1) === ' ') ? position : position + 1).concat(trimSpaces(text.slice(position + 1)))
+	return evaluateValidPosition(position,evaluate,text)
 }
 
 export let commandsWithTwoDigits: stringTransformer = (character: string) => (text: string) => {
-	var position = text.indexOf(character)
-	if (position !== -1) {
-		if (isNaN(+text.charAt(position + 2)) || text.charAt(position + 2) === " ") {
-			return [text.slice(0, position + 1), "0"].join('').concat(commandsWithTwoDigits(character)(text.slice(position + 1)))
-		}
-		else { return text.slice(0, position + 1).concat(commandsWithTwoDigits(character)(text.slice(position + 1))) }
-	}
-	return text
+	const position = text.indexOf(character)
+	const condition = isNaN(+text.charAt(position + 2)) || text.charAt(position + 2) === ' '
+	return evaluateValidPosition(position,() => text.slice(0, position + 1).concat(condition ? '0' : '').concat(commandsWithTwoDigits(character)(text.slice(position + 1))),text)
 }
 
-export function removeSemicolons(text: string): string {
-	if (text.slice(text.length - 1) === ";") {
-		return removeSemicolons(text.slice(0, -1))
-	}
-	else { return text }
-}
-export function splitBySemicolons(text: string): string {
-	var position = text.indexOf(";")
-	if (text.indexOf(";") !== -1) {
-		return [text.slice(0, position), "\n", splitBySemicolons(text.slice(position + 1)).trim()].join('')
-	}
-	else { return text }
-}
+export const removeSemicolons = (text: string): string => (text.slice(text.length - 1) === ';') ? removeSemicolons(text.slice(0, -1)) : text
+
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,36 +7,36 @@ import * as GcodeFormatterExtension from '../../extension'
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.')
-	const line = "G3X100.08Y3.11I28.91;J4.36;"
-	const lineSpaced = "G3 X100.08 Y3.11 I28.91 J4.36;"
-	const lineSuperSpaced = "G3  X100.08    Y3.11    I28.91   J4.36;"
+	const line = 'G3X100.08Y3.11I28.91;J4.36;'
+	const lineSpaced = 'G3 X100.08 Y3.11 I28.91 J4.36;'
+	const lineSuperSpaced = 'G3  X100.08    Y3.11    I28.91   J4.36;'
 
 	test('If Code is at the start, does not add a space', () => {
-		assert.equal("G3X100.08Y3.11I28.91;J4.36;", GcodeFormatterExtension.intercalate("G")(line))
+		assert.equal('G3X100.08Y3.11I28.91;J4.36;', GcodeFormatterExtension.intercalate('G')(line))
 	})
 	test('If Code is not at the start, add a space', () => {
-		assert.equal("G3 X100.08Y3.11I28.91;J4.36;", GcodeFormatterExtension.intercalate("X")(line))
+		assert.equal('G3 X100.08Y3.11I28.91;J4.36;', GcodeFormatterExtension.intercalate('X')(line))
 	})
 	test('If Code is tabulated, then remains equals', () => {
-		assert.equal("G3 X100.08 Y3.11 I28.91 J4.36;", GcodeFormatterExtension.intercalate("X")(lineSpaced))
+		assert.equal('G3 X100.08 Y3.11 I28.91 J4.36;', GcodeFormatterExtension.intercalate('X')(lineSpaced))
 	})	
 	test('If line had a semicolon at the end of line, the last semicolon is removed', () => {
-		assert.equal("G3X100.08Y3.11I28.91;J4.36", GcodeFormatterExtension.removeSemicolons(line))
+		assert.equal('G3X100.08Y3.11I28.91;J4.36', GcodeFormatterExtension.removeSemicolons(line))
 	})
 	test('If line had more than one semicolon at the end of line, all semicolons are removed', () => {
-		assert.equal("G3X100.08Y3.11I28.91;J4.36", GcodeFormatterExtension.removeSemicolons(line.concat(";;;")))
+		assert.equal('G3X100.08Y3.11I28.91;J4.36', GcodeFormatterExtension.removeSemicolons(line.concat(';;;')))
 	})
 	test('If line with no semicolon at the end of line, then the line remains equals', () => {
-		assert.equal("G3X100.08Y3.11I28.91;J4.36", GcodeFormatterExtension.removeSemicolons(line.slice(0, -1)))
+		assert.equal('G3X100.08Y3.11I28.91;J4.36', GcodeFormatterExtension.removeSemicolons(line.slice(0, -1)))
 	})
 	test('If line had a semicolon at the middle of line, then the line is splitted', () => {
-		assert.equal("G3X100.08Y3.11I28.91\nJ4.36", GcodeFormatterExtension.splitBySemicolons(GcodeFormatterExtension.removeSemicolons(line)))
+		assert.equal('G3X100.08Y3.11I28.91\nJ4.36', GcodeFormatterExtension.splitBySemicolons(GcodeFormatterExtension.removeSemicolons(line)))
 	})
 	test('If Code has one digit, then formats to two digits', () => {
-		assert.equal("G03X100.08Y3.11I28.91;J4.36;", GcodeFormatterExtension.commandsWithTwoDigits("G")(line))
+		assert.equal('G03X100.08Y3.11I28.91;J4.36;', GcodeFormatterExtension.commandsWithTwoDigits('G')(line))
 	})
 	test('If Code has two digits, then remains equals', () => {
-		assert.equal("G03", GcodeFormatterExtension.commandsWithTwoDigits("G")("G03"))
+		assert.equal('G03', GcodeFormatterExtension.commandsWithTwoDigits('G')('G03'))
 	})
 	test('If line has more than one space, then trim useless spaces', ()=>{
 		assert.equal(lineSpaced,GcodeFormatterExtension.trimSpaces(lineSuperSpaced))
